7ff5d8d5f274e99bc7c7ef358c0b2dab301e77e4
Yousef Elsayed <yousef4511@outlook.com>
2024-08-05 16:06:25 +0300

    Add docker & git extras to nvim

--- .config/nvim/lua/config/lazy.lua
+++ .config/nvim/lua/config/lazy.lua
@@ -26,6 +26,8 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.lang.tailwind" },
     { import = "lazyvim.plugins.extras.lang.markdown" },
     { import = "lazyvim.plugins.extras.coding.mini-surround" },
+    { import = "lazyvim.plugins.extras.lang.docker" },
+    { import = "lazyvim.plugins.extras.lang.git" },
     { import = "plugins" },
   },
   defaults = {
