cf1c5774091a6c01602b0914e9d44e93b48bec31
Yousef Elsayed <yousef4511@outlook.com>
2024-09-16 13:06:06 +0300

    Add new wallpaper and change wezterm config accordingly

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -5,7 +5,7 @@ local wezterm = require("wezterm")
 local config = wezterm.config_builder()
 
 -- Appearance settings
-local background_image_path = os.getenv("HOME") .. "/.dotfiles/blurred-wallpapers/8-blur.jpeg"
+local background_image_path = os.getenv("HOME") .. "/.dotfiles/blurred-wallpapers/dark-red-win-11-blur.png"
 
 config.font = wezterm.font("Iosevka Nerd Font Mono")
 config.font_size = 14.0
@@ -13,12 +13,12 @@ config.enable_tab_bar = false
 
 -- Window background settings
 config.window_background_image = background_image_path
-config.window_background_image_hsb = {
-	brightness = 0.3,
-	-- hue = 1.0,
-	-- saturation = 1.0,
-}
-config.text_background_opacity = 0.3
+-- config.window_background_image_hsb = {
+-- brightness = 0.3,
+-- hue = 1.0,
+-- saturation = 1.0,
+-- }
+-- config.text_background_opacity = 0.3
 
 -- Optionally set window opacity (uncomment if needed)
 -- config.window_background_opacity = 0.75
