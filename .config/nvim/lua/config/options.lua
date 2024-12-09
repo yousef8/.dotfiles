4f444dc178a622e8c8e0e53a2b36304f78dd15ce
Yousef Elsayed <yousef4511@outlook.com>
2024-12-09 12:55:16 +0200

    feat: use fzf for telescope & install lsp-signature

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -5,3 +5,7 @@
 -- Enable the option to require a Prettier config file
 -- If no prettier config file is found, the formatter will not be used
 vim.g.lazyvim_prettier_needs_config = false
+
+-- In case you don't want to use `:LazyExtras`,
+-- then you need to set the option below.
+vim.g.lazyvim_picker = "fzf"
