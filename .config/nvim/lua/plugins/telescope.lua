176c00331ff333c05715337e49c1bf6630ff1772
Yousef Elsayed <yousef4511@outlook.com>
2024-12-17 08:56:56 +0200

    feat(nvim): remove telescope plugin

--- .config/nvim/lua/plugins/telescope.lua
+++
@@ -1,8 +0,0 @@
-return {
-  "nvim-telescope/telescope.nvim",
-  keys = {
-    -- add/override keymaps
-    { "<leader>ff", require("telescope.builtin").find_files, desc = "Find Files (cwd)" },
-    { "<leader><space>", require("telescope.builtin").find_files, desc = "Find Files (cwd)" },
-  },
-}
