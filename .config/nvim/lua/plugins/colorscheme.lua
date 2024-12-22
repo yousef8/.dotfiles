05dcb783adba03eb8b257c35e1b784867c33cd7a
Yousef Elsayed <youssef.ghonim@fixed.global>
2024-12-22 21:11:49 +0200

    feat(nvim): use tokyonight colorscheme

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -1,37 +1,5 @@
 return {
   {
-    "bluz71/vim-moonfly-colors",
-    name = "moonfly",
-    -- opts = {
-    --   colorscheme = function()
-    --     require("vim-moonfly-colors").load()
-    --   end,
-    -- },
-  },
-  -- {
-  --   "catppuccin/nvim",
-  --   opts = {
-  --     color_overrides = {
-  --       mocha = {
-  --         base = "#000000",
-  --         mantle = "#000000",
-  --         crust = "#000000",
-  --       },
-  --     },
-  --   },
-  -- },
-  {
-    "folke/tokyonight.nvim",
-    opts = {
-      style = "night",
-      transparent = true, -- It does nothing styles property is what makes everything transparent
-      styles = {
-        sidebars = "transparent",
-        floats = "transparent",
-      },
-    },
-  },
-  {
     "LazyVim/LazyVim",
     opts = {
       colorscheme = "tokyonight",
