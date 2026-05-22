08cb0057a9b425bbfdd3f0ff7a6d3e78bfc5ee41
Yousef Elsayed <yousef4511@outlook.com>
2026-05-22 18:17:30 +0300

    feat(nvim): rm rose-pine colortheme and set tokyonight-night default

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -1,11 +1,10 @@
 return {
   { "rebelot/kanagawa.nvim" },
   { "EdenEast/nightfox.nvim" },
-  { "rose-pine/neovim" },
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "rose-pine",
+      colorscheme = "tokyonight-night",
     },
   },
 }
